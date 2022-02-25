81e38061d0c54dbd8ec921da187e4e00db72fab5
gerbelli27 <guerbell@hotmail.com>
2022-02-25 10:32:54 +0000

    project react rocketseat

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Firebase Connected</h1>
+}
+
+export default App
---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_AP_API_KEY,
+  authDomain: import.meta.env.VITE_AP_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_AP_DATABASE_URL,
+  projectId: import.meta.env.VITE_AP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_AP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_AP_MESSAGINS_SENDER_ID,
+  appId: import.meta.env.VITE_AP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
